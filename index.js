8109fd41b0d42bbe62f1d3cea2894a8d3e007d5d
smallgear <northwindapps@gmail.com>
2019-11-16 21:01:42 +0900

    wait..

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: './uploads',
     filename: function(req, file, cb){
       cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
