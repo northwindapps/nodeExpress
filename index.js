f1dd600c3a908d6b27da97dd15621c9353df38ce
smallgear <northwindapps@gmail.com>
2019-11-16 15:07:34 +0900

    cors

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', (req,res)=> {res.send("You have got promoted.")
 });
 var port = process.env.PORT || 3000;
