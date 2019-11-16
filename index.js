76ea8a2d90e34c5aed595e592eeb9db9c880fde8
smallgear <northwindapps@gmail.com>
2019-11-16 14:58:10 +0900

    try again

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.get('/', (req,res)=> {res.console.log("Here is your meal.")} );
+app.get('/', (req,res)=> {res.send("You have got promoted.")
+});
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
  console.log("Server running on port 3000");
